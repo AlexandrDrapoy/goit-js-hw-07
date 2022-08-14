7f799e901e9fccebf625af86fe62d40a4755d476
Alexander Drapoy <dr4alex@gmail.com>
2022-08-14 15:04:35 +0300

    finish 7HW

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryImg = document.querySelector(".gallery");
+
+const galleryMurkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`;
+  })
+  .join("");
+
+galleryImg.insertAdjacentHTML("beforeend", galleryMurkup);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
